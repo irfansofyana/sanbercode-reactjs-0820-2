d0f4f18ce5465bff0655efbaa5a372182224eacb
irfansofyana <irfansofyana0305@gmail.com>
2020-09-07 21:41:46 +0700

    Add Tugas-11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FormPembelian from  './Tugas-9/FormPembelian';
 import TabelHargaBuah from './Tugas-10/TabelHargaBuah';
+import Timer from './Tugas-11/Timer';
 
 function App() {
   return (
     <div>
       <FormPembelian />
       <TabelHargaBuah />
+      <Timer countdown='101'/>
     </div>
   );
 }
---
+++ src/Tugas-11/Timer.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import { unmountComponentAtNode } from "react-dom";
+
+class Timer extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      countdown: props.countdown
+    }
+  }
+
+  componentDidMount(){
+    if (this.props.countdown !== undefined){
+      this.setState({countdown: this.props.countdown})
+    }
+    this.timer = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+
+
+  tick() {
+    this.setState({
+      countdown: this.state.countdown - 1 
+    });
+  }
+
+
+  componentDidUpdate() {
+    if (this.state.countdown === 0) {
+        this.render = () => null;
+    }
+  }
+
+  render(){
+    return(
+      <div style={{display: 'flex', textAlign: 'center'}}>
+        <h1 style={{margin: '2% auto'}}>sekarang jam: {
+            new Date().toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+                hour12: true
+            })
+        }
+        </h1>
+          <h1 style={{margin: '2% auto'}}>
+              hitung mundur: {this.state.countdown}
+          </h1>
+      </div>
+    )
+  }
+}
+
+export default Timer
